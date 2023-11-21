7c61bd7251f6d0aea367c17cdc1453499822ab46
Nurtilek <nurticcip7@gmail.com>
2023-11-21 15:42:36 +0600

    example

---
+++ script.js
@@ -0,0 +1,66 @@
+// ! Function decloration
+// function myFunc(str) {
+//     return str
+// }
+// console.log(myFunc('motion'))
+
+
+
+// ! Function expression
+// const myFn = function () {
+//     return 'hello world!'
+// }
+// console.log(myFn())
+
+
+
+// ! Arrow function =>
+// const myFunc = () => {
+//     return 'hello world!'
+// }
+// console.log(myFunc())
+
+
+
+// function myFunc(str) {
+//     if (str[0] === 'n') {
+//         return str + 'academy';
+//     }
+//     return 'web';
+// }
+// console.log(myFunc('motion'))
+
+
+// function plusNumbers(num) {
+//     return num ** 2
+// }
+// console.log(plusNumbers(100))
+
+
+
+// function square(number) {
+//     let result = number ** 2
+//     console.log(result)
+// }
+// square(10)
+
+
+
+
+//?
+// function numbers(numberOne, numberTwo) {
+//     return numberOne ** numberTwo
+// }
+// console.log(numbers(2,3))
+
+
+// const numbers = function (numberOne, numberTwo) {
+//     return numberOne ** numberTwo
+// }
+// console.log(numbers(2,3))
+
+
+// const numbers = (numberOne, numberTwo) => {
+//     return numberOne ** numberTwo
+// }
+// console.log(numbers(2, 3))
